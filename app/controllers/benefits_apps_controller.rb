59caea1bfdc99d6d07f5f296712836281e19a3a1
Tahsina Islam <tahsina@codeforamerica.org>
2022-10-11 19:52:18 -0400

    Create benefits app

---
+++ app/controllers/benefits_apps_controller.rb
@@ -0,0 +1,20 @@
+class BenefitsAppsController < ApplicationController
+  def index
+    @benefits_apps = BenefitsApp.all
+  end
+
+  def new
+    @benefits_app = BenefitsApp.new
+  end
+
+  def create
+    @benefits_app = BenefitsApp.new
+
+    if @benefits_app.save
+      redirect_to action: "index"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+end
