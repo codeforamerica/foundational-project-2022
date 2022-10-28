15b883be1aa61fba07fd76c4f671ee56cc4c23c7
Tahsina Islam <tahsina@codeforamerica.org>
2022-10-28 18:00:57 -0400

    darn-links
    Signed-off-by: Nishi Nelson <nnelson@codeforamerica.org>

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
 
   patch "benefits/:id/update_date" => "benefits_apps#update_date", :as => "update_date"
+  mount Cfa::Styleguide::Engine => "/cfa"
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
