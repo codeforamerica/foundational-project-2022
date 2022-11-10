4454f93d49e96c2e26522a6fd80db1057f0c0518
Tahsina Islam <tahsina@codeforamerica.org>
2022-11-10 17:38:55 -0500

    Add tests for models and controllers
    Signed-off-by: Nishi Nelson <nnelson@codeforamerica.org>

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -5,31 +5,28 @@ describe MembersController do
   let(:benefits_app){BenefitsApp.create(email_address:'some_email@email.com')}
   context '#create' do
     it 'creates members object when all field valid' do
-      members_before = benefits_app.members.count
-      benefits_app.members.create(first_name: 'First Name', last_name: 'Last Name', birthdate: '10/10/2000')
-      members_after = benefits_app.members.count
-      expect(members_after).to eq(members_before + 1)
+      expect do
+        benefits_app.members.create(first_name: 'First Name', last_name: 'Last Name', birthdate: '10/10/2000')
+      end.to change { benefits_app.members.count }.by(1)
     end
-  end
 
-  it 'does not make a member if birthdate is not in a valid format' do
-    members_before = benefits_app.members.count
-    benefits_app.members.create(first_name: 'First Name', last_name: 'Last Name', birthdate: '10/10/00')
-    members_after = benefits_app.members.count
-    expect(members_after).to eq(members_before)
-  end
+    it 'does not make a member if birthdate is not in a valid format' do
+      expect do
+        benefits_app.members.create(first_name: 'First Name', last_name: 'Last Name', birthdate: '10/10/00')
+      end.not_to change { benefits_app.members.count }
+    end
 
-  it 'does not make a member if first name is not inputted' do
-    members_before = benefits_app.members.count
-    benefits_app.members.create(last_name: 'Last Name', birthdate: '10/10/2000')
-    members_after = benefits_app.members.count
-    expect(members_after).to eq(members_before)
-  end
+    it 'does not make a member if first name is not inputted' do
+      expect do
+        benefits_app.members.create(last_name: 'Last Name', birthdate: '10/10/2000')
+      end.not_to change { benefits_app.members.count }
+    end
 
-  it 'does not make a member if last name is not inputted' do
-    members_before = benefits_app.members.count
-    benefits_app.members.create(first_name: 'First Name', birthdate: '10/10/2000')
-    members_after = benefits_app.members.count
-    expect(members_after).to eq(members_before)
+    it 'does not make a member if last name is not inputted' do
+      expect do
+        benefits_app.members.create(first_name: 'First Name', birthdate: '10/10/2000')
+      end.not_to change { benefits_app.members.count }
+
+    end
   end
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 #testing logic for primary and secondary members
 RSpec.describe Member, :type => :model do
   context 'if new member is first member' do
-    bennie_app = BenefitsApp.new(email_address: 'some@email.com')
-    bennie_app.save
-    member = bennie_app.members.create(birthdate: "12/12/2002", first_name: "name", last_name: "last")
-    member2 = bennie_app.members.create(birthdate: "12/12/2022", first_name: "name2", last_name: "last2")
+    let!(:bennie_app){BenefitsApp.create(email_address:'some@email.com')}
+    let!(:member1){bennie_app.members.create(birthdate: "12/12/2022", first_name: "name1", last_name: "last1")}
+    let!(:member2){bennie_app.members.create(birthdate: "12/12/2022", first_name: "name2", last_name: "last2")}
+
     it 'sets primary_member to true' do
-      expect(member.primary_member).to eq(true)
+      expect(member1.primary_member).to eq(true)
     end
 
     it 'sets primary_member to false for subsequent members' do
