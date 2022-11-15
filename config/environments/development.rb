453be2f7b7f447a327ce17736e16606699b1207d
Nishi Nelson <nnelson@codeforamerica.org>
2022-11-14 16:35:56 -0800

    Adding config.hosts to development environment
    Co-authored-by: Tahsina Islam <tahsina@codeforamerica.org>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.hosts << "app-47822.on-aptible.com"
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
