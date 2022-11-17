9722ecbbfa15f20df97a486eb6b4f696c71ed918
Tahsina Islam <tahsina@codeforamerica.org>
2022-11-17 18:34:47 -0500

    Fix income associations
    Signed-off-by: Nishi Nelson <nnelson@codeforamerica.org>

--- app/models/benefits_app.rb
+++ app/models/benefits_app.rb
@@ -1,6 +1,6 @@
 class BenefitsApp < ApplicationRecord
   has_many :members, dependent: :destroy
-  has_one :income, dependent: :destroy
+  has_many :incomes, dependent: :destroy
   validates :email_address, :format => { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Please enter a valid email address"}
   # validates_presence_of :email_address, message: 'This field is required'
   def update_date_of_submission
