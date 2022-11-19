ecbd8b6a88135f209636da42be39032c3381d086
Nishi Nelson <nnelson@codeforamerica.org>
2022-11-18 17:35:22 -0800

    Added buttons to new incomes with some functionality. still working on it
    Signed-off-by: Tahsina Islam <tahsina@codeforamerica.org>
    Co-authored-by: Tahsina Islam <tahsina@codeforamerica.org>

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -3,4 +3,28 @@ class IncomesController < ApplicationController
     @benefits_app = BenefitsApp.find(params[:benefits_app_id])
     @income = @benefits_app.incomes.new
   end
+
+  def index
+
+  end
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def income_info
+    redirect_to benefits_app_income_path
+  end
+
+  def employed
+    @benefits_app = BenefitsApp.find(params[:benefits_app_id])
+    @income = @benefits_app.incomes.create(income_params)
+  end
+
+  def income_params
+    params.require(:income).permit(:has_income, :income, :income_biweekly, :income_monthly, :total_income)
+  end
 end
