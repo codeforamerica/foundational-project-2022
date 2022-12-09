a90c55e6b3e793243faa4336a367423eecb021ff
Nishi Nelson <nnelson@codeforamerica.org>
2022-12-09 14:10:48 -0800

    Fixed rspec issue by adding additonal params depth: { params } => { benefits_apps: { params } }
    Co-authored-by: Tahsina Islam <tahsina@codeforamerica.org>

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -4,8 +4,13 @@ class SignaturesController < ApplicationController
   end
 
   def update
+    # When a controller action runs, Rails has set up a params value that is the
+    # data submitted to this page from this request.
+    #
+    # Extra info, feel free to ignore: That can come from a form submission or the query string,
+    # and it also includes some data Rails thinks you might want that the user didn't specifically submit.
     @benefits_app = BenefitsApp.find(params[:id])
-
+    puts "======================= #{params}"
     if @benefits_app.update(benefits_app_params)
       @benefits_app.update_date_of_submission
       redirect_to root_path
@@ -16,7 +21,11 @@ class SignaturesController < ApplicationController
 
   private
   def benefits_app_params
-    params.permit(:benefits_app_id, :email_address, :signature)
+    # params.permit(:benefits_app_id, :email_address, :signature)
+    params.require(:benefits_app).permit( :id, :benefits_app_id, :email_address, :signature)
+    # sample input/output pair?
+    # params = { "benefits_app": { id: 1, benefitsapp_id: 1 }, "user": {name: "New Name"} }
+    # output = ... { id: 1}
   end
 end
 
--- spec/controllers/signatures_controller_spec.rb
+++ spec/controllers/signatures_controller_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 describe SignaturesController do
   let!(:benefits_app){BenefitsApp.create!(email_address:'some_email@email.com')}
-  let(:params) { {id: benefits_app.id, signature: signature} }
+  let(:params) do
+    { id: benefits_app.id, benefits_app: {id: benefits_app.id, signature: signature, benefits_app: benefits_app} }
+  end
   let(:signature) { "Joe Smith" }
   context 'signature is entered and submit its pressed' do
     it 'saves signature and reroutes to route page' do
+      # puts "++++++++++++++++++++#{params}"
       expect(patch :update, params: params).to redirect_to(root_path)
       expect(assigns(:benefits_app).signature).to eq("Joe Smith")
     end
